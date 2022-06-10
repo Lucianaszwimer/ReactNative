f426c8cd0f80194bdb77de256aac22753a44d9d4
LUCIANA YAEL SZWIMER <46195270@ort>
2022-06-10 12:16:31 -0300

    Pelotudsa

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, Text, TextInput } from 'react-native';
+import ScrollViewCommands from 'react-native/Libraries/Components/ScrollView/ScrollViewCommands';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Image source={{ uri: 'https://www.ort.edu.ar/img/LogoOrtArgWeb2017.jpg' }}
+          style={{
+            width: 260,
+            height: 150,
+          }}
+          />
+        <Text style={styles.text}>Iniciar Sesion</Text>
+
+      <View style={{alignItems: 'flex-start'}}>
+        <TextInput style={styles.input}
+          //onChangeText={text => onChangeText(text)}
+          value={Number} placeholder="Ingrese su CUIL          ">
+        </TextInput>
+
+        <TextInput style={styles.input}
+          //onChangeText={text => onChangeText(text)}
+          value={Text} placeholder="Ingrese su contraseña">
+        </TextInput>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +35,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 70,
     alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  text: {
+    color: '#fff',
+    marginTop: 60,
+    fontSize: 25,
+    padding: 7,
+    paddingHorizontal: 40,
+    borderRadius: 20,
+    backgroundColor: '#202e99',
+  },
+
+  input: {
+    height: 40,
+    margin: 40,
+    borderColor: 'grey',
+    borderBottomWidth: 2,
+    paddingHorizontal: 50,
+  }
+
 });
