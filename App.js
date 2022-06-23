8e2a212f71121987ff6e29f7b921fdce75c7ed56
Lucianaszwimer <91558510+Lucianaszwimer@users.noreply.github.com>
2022-06-23 15:19:40 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Image, Text, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, View, Image, Text, TextInput, Button, Alert, SectionList } from 'react-native';
 import ScrollViewCommands from 'react-native/Libraries/Components/ScrollView/ScrollViewCommands';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
 
-export default function App() {
+// página para iniciar sesion
+function LoginScreen() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-        <Image source={{ uri: 'https://www.ort.edu.ar/img/LogoOrtArgWeb2017.jpg' }}
-          style={{
-            width: 260,
-            height: 150,
-          }}
-          />
-        <Text style={styles.text}>Iniciar Sesion</Text>
+      <Image source={{ uri: 'https://www.ort.edu.ar/img/LogoOrtArgWeb2017.jpg' }}
+        style={{
+          width: 260,
+          height: 150,
+        }}
+      />
+      <Text style={styles.text}>Iniciar Sesion</Text>
 
-      <View style={{alignItems: 'flex-start'}}>
+      <View style={{ alignItems: 'flex-start' }}>
         <TextInput style={styles.input}
           //onChangeText={text => onChangeText(text)}
           value={Number} placeholder="Ingrese su CUIL          ">
@@ -26,16 +31,98 @@ export default function App() {
         </TextInput>
       </View>
 
-      <Button 
+      <Button
+        title="Log in"
+        color="#525252"
+        onPress={() => navigation.navigate('Presente')}
+        //onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+      <Text style={styles.minitext}>¿No tenes una cuenta ya registrada?
+        <Button
+          title="Presiona aquí para registrarte"
+          onPress={() => navigation.navigate('Signin')}
+        />
+      </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+//página para registrarse
+function SigninScreen() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: 'https://www.ort.edu.ar/img/LogoOrtArgWeb2017.jpg' }}
+        style={{
+          width: 260,
+          height: 150,
+        }}
+      />
+      <Text style={styles.text}>Registrate</Text>
+
+      <View style={{ alignItems: 'flex-start' }}>
+        <TextInput style={styles.input}
+          //onChangeText={text => onChangeText(text)}
+          value={Number} placeholder="Ingrese su CUIL          ">
+        </TextInput>
+
+        <TextInput style={styles.input}
+          //onChangeText={text => onChangeText(text)}
+          value={Text} placeholder="Ingrese su contraseña">
+        </TextInput>
+      </View>
+
+      <Button
         title="Sign In"
         color="#525252"
         onPress={() => Alert.alert('Simple Button pressed')}
       />
 
-      <Text style={styles.minitext}>¿No tenes una cuenta ya registrada?</Text>
+      <Text style={styles.minitext}>¿Ya tenes una cuenta?
+        <Button
+          title="Presiona aquí para iniciar sesion"
+          onPress={() => navigation.navigate('Login')}
+        />
+      </Text>
 
-      <StatusBar style="auto" />
     </View>
+
+  );
+}
+
+//página del paresente/ausente/tarde 
+function PresenteScreen() {
+  //const navigation = useNavigation();
+  return (
+    <View>
+      <SectionList
+          sections={[
+            {title: 'D', data: ['Devin', 'Dan', 'Dominic']}
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+    </View>
+
+
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signin" component={SigninScreen} />
+        <Stack.Screen name="Presente" component={PresenteScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -66,10 +153,12 @@ const styles = StyleSheet.create({
   },
 
   minitext: {
-    marginTop: 40, 
+    marginTop: 40,
     color: 'white',
     padding: 50,
     backgroundColor: '#202e99'
   }
 
 });
+
+
